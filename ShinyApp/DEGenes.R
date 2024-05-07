2feeb1d28403607cdd2b42dc8fd2af8bac930c1a
TimBr <tim.bruggeman@student.howest.be>
2024-05-07 17:04:44 +0200

    Made a script to get an RDSObject to make datatable with p-values per gene per celltype for the WT_data

---
+++ ShinyApp/DEGenes.R
@@ -0,0 +1,33 @@
+### Diff gene analyse, script to make an RDSObject from with the output ###
+###########################################################################
+
+## load in packages##
+#####################
+library(Seurat)
+
+
+## Loading in the data ##
+#########################
+#annot <- readRDS("/kyukon/data/gent/vo/000/gvo00027/PPOL/SharedData/2024_TimBruggeman/RDSObjects/ada_and_stefNoALK_dim20_res06_ANNOT.rds")
+cell <- readRDS("/kyukon/data/gent/vo/000/gvo00027/PPOL/SharedData/2024_TimBruggeman/RDSObjects/CellsOfInterest_SLB.rds")
+ALK <- readRDS("/kyukon/data/gent/vo/000/gvo00027/PPOL/SharedData/2024_TimBruggeman/RDSObjects/stef_ALK_and_noALK_integrated_SLB.rds")
+
+
+## diff gene analyse ##
+#######################
+Idents(cell) <- "CellType"
+
+DimPlot(cell, reduction = "umap", label = TRUE)
+
+# Define your comparison groups
+comparison_groups <- c("Sympathoblasts", "CPC", "SCP", "ProlifSympathoblasts", "Bridging Cells")
+
+# Initialize an empty list to store the results
+all_results <- list()
+
+# Loop through each comparison group and perform FindMarkers
+for (group in comparison_groups) {
+  result <- FindMarkers(cell, ident.1 = group)
+  all_results[[group]] <- result
+}
+saveRDS(all_results, file = "DEGenes.RDS")
