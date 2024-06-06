3c67a60f0092a170ed1f314eec5e20846b59c0cb
TimBr <tim.bruggeman@student.howest.be>
2024-06-06 15:33:32 +0200

    update DEGenes.R

--- ShinyApp/DEGenes.R
+++ ShinyApp/DEGenes.R
@@ -10,11 +10,10 @@ library(Seurat)
 #########################
 #annot <- readRDS("/kyukon/data/gent/vo/000/gvo00027/PPOL/SharedData/2024_TimBruggeman/RDSObjects/ada_and_stefNoALK_dim20_res06_ANNOT.rds")
 cell <- readRDS("/kyukon/data/gent/vo/000/gvo00027/PPOL/SharedData/2024_TimBruggeman/RDSObjects/CellsOfInterest_SLB.rds")
-ALK <- readRDS("/kyukon/data/gent/vo/000/gvo00027/PPOL/SharedData/2024_TimBruggeman/RDSObjects/stef_ALK_and_noALK_integrated_SLB.rds")
 
 
-## WT diff gene analyse ##
-##########################
+## diff gene analyse ##
+#######################
 Idents(cell) <- "CellType"
 
 DimPlot(cell, reduction = "umap", label = TRUE)
@@ -30,20 +29,5 @@ for (group in comparison_groups) {
   result <- FindMarkers(cell, ident.1 = group)
   all_results[[group]] <- result
 }
+# saving the data in a .rds file
 saveRDS(all_results, file = "DEGenes.RDS")
-
-## ALK diff gene analyse ##
-###########################
-Idents(ALK) <- c("ALK","SC")
-DimPlot(ALK, reduction = "umap", label = TRUE)
-UMAPPlot(ALK, group.by = "Celltype")
-
-comparison_groups <- c("ALK", "SC")
-all_ALK_results <- list()
-
-# Loop through each comparison group and perform FindMarkers
-for (group in comparison_groups) {
-  result <- FindMarkers(ALK, ident.1 = group)
-  all_ALK_results[[group]] <- result
-}
-saveRDS(all_ALK_results, file = "DEGenes_ALK.RDS")
